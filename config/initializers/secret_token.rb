ab79010b65b8bbab08bbe9ecc4a99a92d035f829
shahin70000 <shahin70000@gmail.com>
2013-02-18 09:39:44 +0300

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperclip::Application.config.secret_token = '2c9aba064cfe995bb4a7b7075760620d3222e12514060c5c855016d211239a55619040f30857ac304853eba0505bfb87539fd8290d9cb5f52cda76faa4f6df05'
